1de936e1c24777115dd0d49f0e055c65afabc161
xxw-git <18736071804@163.com>
2019-07-21 13:22:49 +0800

    SpringMVC 配置swagger2

--- src/main/java/com/lc/xxw/swagger/Swagger2Config.java
+++ src/main/java/com/lc/xxw/swagger/Swagger2Config.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
